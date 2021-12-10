7a292ed48214b68af8753650c2571d5968d289a9
Astha Gupta <astha736@gmail.com>
2021-12-10 20:08:31 +0100

    [FIX] Multi-cursor installation issue

--- personal/mycutom.el
+++ personal/mycutom.el
@@ -7,7 +7,8 @@
 
 ;; multiple-cursors requires cl-lib
 (prelude-require-packages '(elpy jedi cl-lib multiple-cursors sphinx-doc))
-(require 'multiple-cursors)
+(prelude-require-packages '(multiple-cursors))
+;; (require 'multiple-cursors)
 
 (elpy-enable)
 (setq elpy-rpc-backend "jedi")
@@ -28,16 +29,25 @@
 (setq-default org-display-custom-times t)
 (setq org-time-stamp-custom-formats '("<%a %b %e %Y>" . "<%a %b %e %Y %H:%M>"))
 
+
+(setq-default multi-cursor-mode t)
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C-S-w C-S-w") 'mc/mark-all-dwim)
+(global-set-key (kbd "C-S-e C-S-e") 'mc/edit-ends-of-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
-;; set the ediff to open difference horizontally 
+(setq mc/cmds-to-run-for-all
+      '(
+        sp-backward-delete-char
+        ))
+
+;; set the ediff to open difference horizontally
 ;; by degfault windows are open vertically
 (custom-set-variables
  '(ediff-split-window-function (quote split-window-horizontally))
  '(ediff-highlight-all-diffs t)
  '(ediff-auto-refine t)
- 
+
 )
